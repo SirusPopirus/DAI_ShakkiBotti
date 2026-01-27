6304382c1e3d6bf899f192be9a4fc74269fa13fb
Leha <aleksi.kirppu@metropolia.fi>
2026-01-27 18:39:32 +0200

    versio1.1

--- shakki/kayttoliittyma.cpp
+++ shakki/kayttoliittyma.cpp
@@ -3,92 +3,183 @@
 #include <string>
 #include <fcntl.h>
 #include <io.h>
-#include <iostream>
 #include "kayttoliittyma.h"
-
+#include "nappula.h"
+#include <algorithm>
+#include <cctype>
 
 using namespace std;
-
 
 Kayttoliittyma* Kayttoliittyma::instance = 0;
 
 Kayttoliittyma* Kayttoliittyma::getInstance()
 {
-	if (instance == 0)
-		instance = new Kayttoliittyma();
-	return instance;
+    if (instance == 0)
+        instance = new Kayttoliittyma();
+    return instance;
 }
+
+static string trim(const string& s) {
+    size_t start = s.find_first_not_of(" \t\r\n");
+    if (start == string::npos) return "";
+    size_t end = s.find_last_not_of(" \t\r\n");
+    return s.substr(start, end - start + 1);
+}
+static bool parseSquare(const string& sq, Ruutu& ruutu)
+{
+    if (sq.size() != 2) return false;
+    char file = tolower(sq[0]);
+    char rank = sq[1];
+    if (file < 'a' || file > 'h') return false;
+    if (rank < '1' || rank > '8') return false;
+    int sarake = file - 'a';
+    int rivi = rank - '1';
+    ruutu = Ruutu(rivi, sarake);
+    return true;
+}
+
 
 void Kayttoliittyma::piirraLauta()
 {
     if (this->_asema == nullptr) return;
 
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-
-    // Pakotetaan Unicode-tila
     _setmode(_fileno(stdout), _O_U16TEXT);
 
-    // Ruutujen värit (TAUSTA + TEKSTI)
-    WORD valkeaRuutu =
-        BACKGROUND_GREEN | BACKGROUND_INTENSITY |
-        FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE;
+    const WORD BG_VALKEA = BACKGROUND_GREEN | BACKGROUND_INTENSITY;
+    const WORD BG_TUMMA = BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY;
 
-    WORD tummaRuutu =
-        BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY |
-        FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE;
+    const WORD FG_VALKOINEN = FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE;
+    const WORD FG_MUSTA = 0;
+
+    const int cellWidth = 3;
 
     wcout << L"\n     a  b  c  d  e  f  g  h" << endl;
 
-    for (int i = 7; i >= 0; i--) {
-
-        // Rivin numero
-        SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+    for (int i = 7; i >= 0; --i) {
+        SetConsoleTextAttribute(hConsole, FG_VALKOINEN);
         wcout << i + 1 << L"  ";
 
-        for (int j = 0; j < 8; j++) {
-
-            // Valitaan ruudun väri
-            SetConsoleTextAttribute(
-                hConsole,
-                ((i + j) % 2) ? tummaRuutu : valkeaRuutu
-            );
+        for (int j = 0; j < 8; ++j) {
+            bool squareIsDark = ((i + j) % 2) != 0;
+            WORD bg = squareIsDark ? BG_TUMMA : BG_VALKEA;
 
             Nappula* n = _asema->_lauta[i][j];
 
             if (n == nullptr) {
-                wcout << L"   ";
+                SetConsoleTextAttribute(hConsole, bg);
+                for (int k = 0; k < cellWidth; ++k) wcout << L' ';
             }
             else {
-                wcout << L" " << n->getUnicode() << L" ";
+                int vari = n->getVari();
+                WORD fg = (vari == 0) ? FG_VALKOINEN : FG_MUSTA;
+
+                bool isPawn = false;
+
+                if (isPawn && vari == 1) {
+                    fg = FG_MUSTA;
+                }
+
+                SetConsoleTextAttribute(hConsole, bg | fg);
+
+                std::wstring piece = n->getUnicode();
+
+                int glyphWidth = (int)piece.length();
+                int padLeft = (cellWidth - glyphWidth) / 2;
+                int padRight = cellWidth - glyphWidth - padLeft;
+
+                for (int k = 0; k < padLeft; ++k) wcout << L' ';
+                wcout << piece;
+                for (int k = 0; k < padRight; ++k) wcout << L' ';
             }
         }
 
-        // Palautetaan normaali väri rivin loppuun
-        SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+        SetConsoleTextAttribute(hConsole, FG_VALKOINEN);
         wcout << L"  " << i + 1 << endl;
     }
 
     wcout << L"     a  b  c  d  e  f  g  h\n" << endl;
-
     _setmode(_fileno(stdout), _O_TEXT);
 }
 
 
-
-/*
-	Aliohjelma tarkistaa ettд kдyttдjдn antama syцte siirroksi on 
-	muodollisesti korrekti (ei tarkista aseman laillisuutta)
-	Ottaa irti myцs nappulan kirjaimen (K/D/L/R/T), tarkistaa ettд kirjain korrekti
-*/
 Siirto Kayttoliittyma::annaVastustajanSiirto()
 {
-	Siirto siirto;
-	return siirto;
-	
-}
+    while (true) {
+        string line;
+        cout << "Anna vastustajan siirto (esim Rg1-f3):  ";
+        getline(cin, line);
 
+        line = trim(line);
+        if (line.empty()) {
+            cout << "tyhjä syöte koita uusiks";
+            continue;
+        }
+
+        string upper = line;
+        transform(upper.begin(), upper.end(), upper.begin(), [](unsigned char c) { return static_cast<char>(toupper(c)); });
+        if (upper == "O-O") {
+            return Siirto(true, false);
+        }
+        if (upper == "O-O-O") {
+            return Siirto(false, true);
+        }
+
+        size_t dash = line.find("-");
+        if (dash == string::npos) {
+            cout << "Virhe ie ole hassua viivaa - : \n";
+            continue;
+        }
+        string left = trim(line.substr(0, dash));
+        string right = trim(line.substr(dash + 1));
+
+        Ruutu alku;
+        Ruutu loppu;
+        bool ok = false;
+
+        if (left.size() == 3) {
+            char piece = static_cast<char>(toupper(static_cast<unsigned char>(left[0])));
+            if (piece == 'T' || piece == 'R' || piece == 'L' || piece == 'D' || piece == 'K' || piece == 'S') {
+                string sq = left.substr(1);
+                if (parseSquare(sq, alku) && parseSquare(right, loppu)) {
+                    ok = true;
+                }
+            }
+            else {
+                cout << "jouu nyt on outo näppäin " << left[0] << "sallitut on vaan: S, T, R, L, D, K.\n";
+            }
+        }
+        else {
+            cout << "virhe pitää olla kirjain + ruutu: esim Rg1\n";
+
+        }
+        if (!ok) {
+            cout << "siirto ei toimínu yritä uusiks\n";
+            continue;
+        }
+        return Siirto(alku, loppu);
+
+    }
+}
 
 int Kayttoliittyma::kysyVastustajanVari()
 {
-	return 0;
+    while (true) {
+        cout << "valkea (v) vai musta (m)? ";
+        string line;
+        getline(cin, line);
+        line = trim(line);
+        if (line.empty()) {
+            cout << "tyhjä syötä yritä uusiks \n";
+            continue;
+        }
+        char c = static_cast<char>(tolower(static_cast<unsigned char>(line[0])));
+        if (c == 'v') {
+            return 0;
+        }
+        if (c == 'm') {
+            return 1;
+        }
+        cout << "Anna 'v' tai 'm'\n";
+    }
 }
--- shakki/ruutu.cpp
+++ shakki/ruutu.cpp
@@ -1,30 +1,27 @@
 #include "ruutu.h"
 
-
-Ruutu::Ruutu(int sarake, int rivi)
+Ruutu::Ruutu(int rivi, int sarake)
 {
+    _sarake = sarake;
+    _rivi = rivi;
 }
-
 
 int Ruutu::getRivi()
 {
-	return 0;
+    return _rivi;
 }
-
 
 int Ruutu::getSarake()
 {
-	return 0;
+    return _sarake;
 }
 
-
-void Ruutu::setRivi(int rivi) 
+void Ruutu::setRivi(int rivi)
 {
-	
+    _rivi = rivi;
 }
 
-
-void Ruutu::setSarake(int sarake) 
+void Ruutu::setSarake(int sarake)
 {
-
+    _sarake = sarake;
 }
